7626064c5d71c28e0e7a50d22149ce3733a81135
syk991116@naver.com <syk991116@naver.com>
2019-09-11 15:00:11 +0900

    update

---
+++ class_20190906.R
@@ -0,0 +1,38 @@
+a = 1
+a
+b = 1
+a == b
+c = 2
+a == c
+class(a)
+a = 'apple'
+a
+a == b
+a = 1
+a <- 1
+a + b
+c = 5
+a + c
+d = a + c
+a = 3
+c = 9
+sqrt(4)
+sum(c(2,3,4))
+?sum
+
+myfunction <- function(a,b){
+  c = a + b
+  return(c)
+}
+
+myfunction(2, 3)
+
+
+
+
+
+
+
+
+
+
---
+++ class_20190909.R
@@ -0,0 +1,22 @@
+a <- c(1,2,3,4)
+class(a)
+b <- c(1L, 2L, 3L)
+class(b)
+length(a)
+c <- c("1", "2", "3", "4")
+class(c)
+length(a) == length(c)
+d = c('E', 'F', 'G')
+e = c(1, 2, 3, 4)
+names(e) <- d
+e
+d
+f = c("E", "F", "G")
+d == f
+
+
+
+
+
+
+
